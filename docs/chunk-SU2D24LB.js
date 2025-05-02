0c82d1209084d550e085c905aabbd5d2bfb4361f
alchiDeveloper3 <thariq@alchidesignstudio.com>
2025-05-02 17:59:02 +0530

    First upload for deployment

---
+++ docs/chunk-SU2D24LB.js
@@ -0,0 +1,2 @@
+import{ca as c,g as t}from"./chunk-EHZKD7YP.js";var i=(()=>{class e{screenSource=new t("Login");currentScreen=this.screenSource.asObservable();constructor(){}changeScreen(r){this.screenSource.next(r)}static \u0275fac=function(o){return new(o||e)};static \u0275prov=c({token:e,factory:e.\u0275fac,providedIn:"root"})}return e})();export{i as a};
+//# sourceMappingURL=chunk-SU2D24LB.js.map
